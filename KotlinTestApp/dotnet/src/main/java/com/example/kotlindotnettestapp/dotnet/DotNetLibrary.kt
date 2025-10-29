749014f9f5d8319d4458b408d72f6ce4ec0dc7ee
Joakim Tall <joakim.tall@bontouch.com>
2025-10-29 15:27:05 +0100

    Internal!

--- KotlinTestApp/dotnet/src/main/java/com/example/kotlindotnettestapp/dotnet/DotNetLibrary.kt
+++ KotlinTestApp/dotnet/src/main/java/com/example/kotlindotnettestapp/dotnet/DotNetLibrary.kt
@@ -4,7 +4,7 @@ import com.sun.jna.Library
 import com.sun.jna.Native
 import com.sun.jna.Pointer
 
-interface DotNetLibrary : Library {
+internal interface DotNetLibrary : Library {
     fun create_hello(): Pointer?
     fun free_string(ptr: Pointer?)
 
