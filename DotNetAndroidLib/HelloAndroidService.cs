b6185c2b0264a09f280a1f6d5bd5a0fc6f5211b4
Joakim Tall <joakim.tall@bontouch.com>
2025-10-29 13:19:19 +0100

    Add handlebars proof

--- DotNetAndroidLib/HelloAndroidService.cs
+++ DotNetAndroidLib/HelloAndroidService.cs
@@ -1,6 +1,7 @@
 using Android.Runtime;
 using Java.Interop;
 using System.Diagnostics.CodeAnalysis;
+using HandlebarsDotNet;
 
 namespace DotNetAndroidLib;
 
@@ -12,6 +13,12 @@ public class HelloAndroidService : Java.Lang.Object
     [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "Exported for Android Callable Wrapper binding.")]
     public virtual string CreateHello()
     {
-        return "Hello from .NET 9 Android!";
+        var template = Handlebars.Compile("🎉 Handlebars says: Hello from {{runtime}} on {{platform}}!");
+        var data = new
+        {
+            runtime = ".NET 9",
+            platform = "Android"
+        };
+        return template(data);
     }
 }
